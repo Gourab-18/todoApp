18c914144392b6f08fa3b5a09568acb274f8433d
Gourab-18 <gourabomm2001@gmail.com>
2024-08-22 18:12:59 +0530

    ft:ui setup

--- frontend/src/ts/appController.ts
+++ frontend/src/ts/appController.ts
@@ -9,12 +9,63 @@ import * as ko from "knockout";
 import * as ResponsiveUtils from "ojs/ojresponsiveutils";
 import * as ResponsiveKnockoutUtils from "ojs/ojresponsiveknockoututils";
 import Context = require("ojs/ojcontext");
+import 'ojs/ojinputtext';
+import * as Bootstrap from 'ojs/ojbootstrap';
+import { ojButton } from 'ojs/ojbutton';
+import 'ojs/ojbutton';
+import 'ojs/ojcheckboxset';
+  import 'ojs/ojtable';
+  import { ojTable } from 'ojs/ojtable';
+  import 'ojs/ojknockout';
+  import 'ojs/ojbutton';
+  import 'ojs/ojoption';
+  import ArrayDataProvider = require('ojs/ojarraydataprovider');
+  import * as empData from "text!./employee_data.json"
+  import { ojMenuEventMap } from 'ojs/ojmenu';
+  import 'ojs/ojmenu';
+
+  interface EmployeeData {
+    EmployeeId: number;
+    EmpName: string;
+    Revenue: number;
+    Rating: number;
+    Status: string;
+  }
+  
 
 class RootViewModel {
   smScreen: ko.Observable<boolean>|undefined;
   appName: ko.Observable<string>;
   userLogin: ko.Observable<string>;
   footerLinks: Array<object>;
+  value: ko.Observable<string>;
+  checkValue: ko.ObservableArray<string>;
+
+    private readonly empArray = ko.observableArray(JSON.parse(empData));
+  
+    readonly dataprovider = new ArrayDataProvider<EmployeeData['EmployeeId'], EmployeeData>(
+      this.empArray,
+      {
+        keyAttributes: 'EmployeeId'
+      }
+    );
+
+    menuListener = (
+      event: ojMenuEventMap['ojMenuAction'],
+      context: ojTable.CellTemplateContext<EmployeeData['EmployeeId'], EmployeeData>
+    ) => {
+      const rowIndex = this.empArray.indexOf(context.item.data);
+      if (event.detail.selectedValue === 'delete') {
+        this.empArray.splice(rowIndex, 1);
+      } else if (event.detail.selectedValue === 'approve') {
+        const rowData = context.item.data;
+        rowData.Status = 'Approved';
+        this.empArray.splice(rowIndex, 1, rowData);
+      }
+    };
+
+    
+
 
   constructor() {
     // media queries for responsive layouts
@@ -22,14 +73,20 @@ class RootViewModel {
     if (smQuery) {
       this.smScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
     }
-
+console.log("Hii")
     // header
 
     // application Name used in Branding Area
     this.appName = ko.observable("App Name");
+    this.value = ko.observable('');
+
 
     // user Info used in Global Navigation area
     this.userLogin = ko.observable("john.hancock@oracle.com");
+
+    console.log("Hii")
+    this.checkValue = ko.observableArray();
+ 
 
     // footer
     this.footerLinks = [
@@ -43,6 +100,21 @@ class RootViewModel {
     // release the application bootstrap busy state
     Context.getPageContext().getBusyContext().applicationBootstrapComplete();        
   }
+  // menuListener = (
+  //     event: ojMenuEventMap['ojMenuAction'],
+  //     context: ojTable.CellTemplateContext<EmployeeData['EmployeeId'], EmployeeData>
+  //   ) => {
+  //     const rowIndex = this.empArray.indexOf(context.item.data);
+  //     if (event.detail.selectedValue === 'delete') {
+  //       this.empArray.splice(rowIndex, 1);
+  //     } else if (event.detail.selectedValue === 'approve') {
+  //       const rowData = context.item.data;
+  //       rowData.Status = 'Approved';
+  //       this.empArray.splice(rowIndex, 1, rowData);
+  //     }
+  //   };
+  // }
+  
 }
 
 export default new RootViewModel();
